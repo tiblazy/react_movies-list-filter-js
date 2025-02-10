31eb8824018390caf67d74ba76b44e814bc7feb3
tiblazy <hauntedapply@gmail.com>
2025-02-10 11:45:43 -0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,61 @@
+import { useState } from 'react';
+import moviesFromServer from './api/movies.json';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
-import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [visibleMovies, setVisibleMovies] = useState([...moviesFromServer]);
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  const searchMovies = data => {
+    setQuery(data);
+
+    if (data !== '') {
+      return setVisibleMovies(
+        [...moviesFromServer].filter(
+          movie =>
+            movie.title
+              .toLowerCase()
+              .trim()
+              .includes(query.toLowerCase().trim()) ||
+            movie.description
+              .toLowerCase()
+              .trim()
+              .includes(query.toLowerCase().trim()),
+        ),
+      );
+    }
+
+    return setVisibleMovies([...moviesFromServer]);
+  };
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                value={query}
+                placeholder="Type search word"
+                onChange={e => searchMovies(e.currentTarget.value)}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
